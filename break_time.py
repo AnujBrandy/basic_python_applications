21adb65cc66164a1ecf637128707ee5de5103101
Anuj Kumar <anashkan123@gmail.com>
2016-02-20 02:08:05 -0800

    Playing the video three times in the browser at every 5 seconds

--- break_time.py
+++ break_time.py
@@ -1,2 +1,12 @@
 import webbrowser
-webbrowser.open( "https://www.youtube.com/watch?v=5klXx8cZbsk" )
+import time
+
+break_count = 0
+total_count = 3
+
+print "The program started at " + str( time.ctime( ) )
+while break_count < total_count :
+	time.sleep( 5 )
+	print "The Program has waited till " + str( time.ctime( ) )
+	webbrowser.open( "https://www.youtube.com/watch?v=5klXx8cZbsk" )
+	break_count = break_count + 1
