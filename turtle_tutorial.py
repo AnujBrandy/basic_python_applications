cfab7e6fdd66ae1ada7e6f69de2289c28ec06b37
Anuj Kumar <anashkan123@gmail.com>
2016-02-21 00:57:13 -0800

    turtle tutorial - moving forward

---
+++ turtle_tutorial.py
@@ -0,0 +1,12 @@
+import turtle
+
+def make_square( ) :
+	window = turtle.Screen( )
+	window.bgcolor( "yellow" )
+
+	brad = turtle.Turtle( )
+	brad.forward( 100 )
+
+	window.exitonclick( )
+
+make_square( )
