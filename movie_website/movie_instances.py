03fbb7c22697d1a393e8913c16bd84e82984f2bc
Anuj Kumar <anashkan123@gmail.com>
2016-02-21 22:58:52 -0800

    Creating classes creating a movie webpage crating template for movies

---
+++ movie_website/movie_instances.py
@@ -0,0 +1,3 @@
+import movie
+
+toy_story = movie.Movie( "Avatar", "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.","https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg" , "https://www.youtube.com/watch?v=5PSNL1qE6VY")
